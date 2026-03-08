cb4b3186ecab3a8ecc6940d51c3db0edca23c7e3
rushil19 <75516786+rushil19@users.noreply.github.com>
2026-03-08 17:48:43 +0000

    Update submitResults.ts

--- submitResults.ts
+++ submitResults.ts
@@ -1,6 +1,6 @@
 // Add 'export' at the beginning!
 export const submitResults = async (formData: any) => { 
-  const response = await fetch("https://script.google.com/macros/s/AKfycbxQKAvJjxg_JUb_90Po8Rm4gyUP9mQxjc3NDnhkqDky1kg--aNrV4UaDpsUap__66mTig/exec", {
+  const response = await fetch("https://script.google.com/macros/s/AKfycbzyn5hGk8VFeTwgbwikqkNssZPBJiOarbtCllduV7GhW2yz67ebsd2tXf6v6qVOeYQIHQ/exec", {
     method: "POST",
     mode: "no-cors",
     // ... the rest of your code ...
