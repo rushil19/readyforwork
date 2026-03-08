26a1ba1b9d1f3ca9a725afa474f5fea7234c9663
rushil19 <75516786+rushil19@users.noreply.github.com>
2026-03-08 17:12:56 +0000

    Update submitResults.ts

--- submitResults.ts
+++ submitResults.ts
@@ -1,4 +1,4 @@
-const response = await fetch(https://script.google.com/macros/s/AKfycbzzdbfMz9fe6B0_UbvYZa6_mv1i8LUo-jrBaMgwRR6KZ1sc70MNgRzwiul3FS6sELjRdQ/exec, {
+const response = await fetch(https://script.google.com/macros/s/AKfycbwNQayBQx873tLzNDUhgG9XDO6pSGzHnSiyLvS0byEgGntlhZ0SM1JMC3dgQh9jiApWQg/exec, {
   method: "POST",
   mode: "no-cors", // Crucial: Tells the browser not to wait for a CORS header
   cache: "no-cache",
